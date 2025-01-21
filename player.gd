8f8dadadd80e981e653edbe50eeac8b4007125a4
Nathan Hebert <hebertnathanj@gmail.com>
2025-01-21 16:20:00 -0700

    Fixed file location

---
+++ player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 300.0
+@export var dash_speed = 10000.0
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+	# Handle dash.
+	if Input.is_action_just_pressed("dash"):
+		velocity = input_direction * dash_speed
+	
+func _physics_process(delta):
+	get_input()	
+	move_and_slide()
